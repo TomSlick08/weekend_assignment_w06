75ad078d1175af3871e80c20ebc04446383eaeb3
TomSlick08 <tommy.threadcraft@gmail.com>
2014-04-07 09:01:09 -0400

    Couldn't get rspec to work, but finished the assignment

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,19 +5,38 @@ class Person < ActiveRecord::Base
 	end
 
 	def birthday
-		#self.birth_date.to_s
+		self.birth_date.to_s
 	end
 
 	def have_a_drink
-
+			age = Date.today.year - self.birth_date.year
+			if age >= 21 
+					if self.drinks < 3
+					self.drinks += 1
+					else 
+						return "Go home you're drunk"
+					end
+			else 
+				return "Wait a few years."
+			end
 	end
 
 	def drive_a_car
-
+		age = Date.today.year - self.birthdate.year
+		if age < 18
+			return "Not yet youngin"
+		elsif age >= 18 && self.licensce == true
+			return true
+		elsif age >= 21 && self.licensce == true && self.drinks > 0
+			return "Looks like a cab for you tonight"
+		end
 	end
 
 	def sober_up
-
+		if self.drinks > 0
+			self.drinks -= 1
+			save
+		end
 	end
 
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,44 @@
 require "spec_helper"
 
 describe Person do
+  
   it "#should return the full name" do
     keith_sweat = Person.create!(first_name: "Keith", last_name: "Sweat")
     expect(keith_sweat.name).to eq("Keith Sweat")
   end
+
+  it "#should return the birthday" do 
+  	david_kim = Person.create(birth_date: "1985-08-31")
+  	expect(david_kim.birth_date).to eq(Sat, 31 Aug 1985)
+	end
+
+	describe "#have_a_drink" do
+		it "should increase drinks attr by 1 if over 21" do 
+			me = Person.create(birth_date: "1986-07-24")
+			me.have_a_drink
+			expect(me.drinks).to eq 1
+		end
+		it "should reject people under 21" do 
+			marcus = Person.create(birthdate: "1998-03-18")
+			expect(marcus.have_a_drink).to eq "Wait a few years"
+		end
+		it "should not let you drink if you already had 3 or more" do
+			ryan = Person.create(birthdate: "1985-05-21")
+			ryan.update_attributes(drinks: 3)
+			expect(person.have_a_drink).to eq "Go home you're drunk"
+		end
+	end
+
+	describe "#drive_a_car" do 
+		it "should reject people who aren't 18 yet " do 
+			marcus = Person.create(birthdate: "1998-03-18")
+			expect(marcus.drive_a_Car).to eq "Not yet youngin"
+	end
+
 end
+
+end
+
+
+end
+
